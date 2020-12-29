7cd8a792761c8740dc50a3c3ead168fcbed1572c
mym0404 <mym0404@gmail.com>
2020-12-29 13:31:19 +0900

    PixelRatio Number 확장 속성들 추가

--- app/src/main/java/team/weathy/util/PixelRatio.kt
+++ app/src/main/java/team/weathy/util/PixelRatio.kt
@@ -2,6 +2,7 @@ package team.weathy.util
 
 import android.app.Application
 import androidx.annotation.Px
+import team.weathy.MainApplication
 import kotlin.math.roundToInt
 
 class PixelRatio(private val app: Application) {
@@ -19,3 +20,15 @@ class PixelRatio(private val app: Application) {
 
     fun toDP(@Px pixel: Int) = (pixel / displayMetrics.density).roundToInt()
 }
+
+val Number.pixel: Int
+    @Px get() = MainApplication.pixelRatio.toPixel(this.toInt())
+
+val Number.dp: Int
+    get() = MainApplication.pixelRatio.toDP(this.toInt())
+
+val Number.pixelFloat: Float
+    @Px get() = MainApplication.pixelRatio.toPixel(this.toInt()).toFloat()
+
+val Number.dpFloat: Float
+    get() = MainApplication.pixelRatio.toDP(this.toInt()).toFloat()
