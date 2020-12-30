ac6c6cf00c1e9e2ccf3647ef1113ce6005a6f06e
dingding-21 <phj8221@naver.com>
2020-12-30 16:48:47 +0900

    설정뷰 각 기능별로 이동

--- app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
+++ app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
@@ -1,8 +1,13 @@
 package team.weathy.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import team.weathy.databinding.ActivitySettingBinding
+import team.weathy.ui.developerinfo.DeveloperInfoActivity
+import team.weathy.ui.inquire.InquireActivity
+import team.weathy.ui.nicknamechange.NicknameChangeActivity
+import team.weathy.util.setOnDebounceClickListener
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
@@ -12,5 +17,34 @@ class SettingActivity : AppCompatActivity() {
 
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        go_nicknameChange()
+        go_inquire()
+        go_developerInfo()
+
+    }
+
+    fun go_nicknameChange(){
+        binding.changeNicknameBtn.setOnDebounceClickListener {
+            val intent = Intent(this, NicknameChangeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun go_inquire(){
+        binding.inquireBtn.setOnDebounceClickListener {
+            val intent = Intent(this, InquireActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun go_developerInfo(){
+        binding.developerInfoBtn.setOnDebounceClickListener {
+            val intent = Intent(this, DeveloperInfoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
