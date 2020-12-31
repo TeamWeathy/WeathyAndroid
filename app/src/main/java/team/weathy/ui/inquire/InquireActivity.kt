8d170a8103f9a2ed6519612d8e199f74f5f80f9d
dingding-21 <phj8221@naver.com>
2021-01-01 01:12:51 +0900

    문의하기 뷰 뒤로가기 버튼 추가

--- app/src/main/java/team/weathy/ui/inquire/InquireActivity.kt
+++ app/src/main/java/team/weathy/ui/inquire/InquireActivity.kt
@@ -1,9 +1,12 @@
 package team.weathy.ui.inquire
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import team.weathy.databinding.ActivityInquireBinding
 import team.weathy.databinding.ActivityLandingBinding
+import team.weathy.ui.setting.SettingActivity
+import team.weathy.util.setOnDebounceClickListener
 
 class InquireActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInquireBinding
@@ -13,5 +16,14 @@ class InquireActivity : AppCompatActivity() {
 
         binding = ActivityInquireBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        exitInquire()
+    }
+
+    private fun exitInquire() {
+        binding.exitInquireBtn.setOnDebounceClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
