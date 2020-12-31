d3ee89e3c59db169b851f969f4326c396a9658fd
dingding-21 <phj8221@naver.com>
2020-12-31 20:13:06 +0900

    스위치 On/Off 표시하도록 구현

--- app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
+++ app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
@@ -2,6 +2,9 @@ package team.weathy.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import team.weathy.databinding.ActivitySettingBinding
 import team.weathy.ui.developerinfo.DeveloperInfoActivity
@@ -18,33 +21,41 @@ class SettingActivity : AppCompatActivity() {
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        go_nicknameChange()
-        go_inquire()
-        go_developerInfo()
-
+        goNicknameChange()
+        goInquire()
+        goDeveloperInfo()
+        pushNotifSwitchAct()
     }
 
-    fun go_nicknameChange(){
+    fun goNicknameChange() {
         binding.changeNicknameClick.setOnDebounceClickListener {
+            Log.d("ddd", "0")
             val intent = Intent(this, NicknameChangeActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 
-    fun go_inquire(){
+    fun goInquire() {
         binding.inquireClick.setOnDebounceClickListener {
             val intent = Intent(this, InquireActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 
-    fun go_developerInfo(){
+    fun goDeveloperInfo() {
         binding.developerInfoClick.setOnDebounceClickListener {
             val intent = Intent(this, DeveloperInfoActivity::class.java)
             startActivity(intent)
-            finish()
+        }
+    }
+
+    fun pushNotifSwitchAct() {
+        binding.pushNotifSwitch.setOnCheckedChangeListener { _, onSwitch ->
+            if (onSwitch) {
+                Toast.makeText(this, "On", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Off", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
