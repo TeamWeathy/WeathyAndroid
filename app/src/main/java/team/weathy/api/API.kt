2ff87763ca8bdbc66480b14efc1d461c8cd60712
mym0404 <mym0404@gmail.com>
2020-12-31 15:19:09 +0900

    Retrofit 기본 설정

---
+++ app/src/main/java/team/weathy/api/API.kt
@@ -0,0 +1,24 @@
+package team.weathy.api
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object API {
+    private val gson = GsonBuilder().create()
+    private val okHttpClient = OkHttpClient.Builder().addNetworkInterceptor(HttpLoggingInterceptor().apply {
+        this.level = HttpLoggingInterceptor.Level.BODY
+    }).addInterceptor {
+        val headerAddedRequest = it.request().newBuilder().addHeader("token", "/*Header*/" /*TODO*/).build()
+
+        it.proceed(headerAddedRequest)
+    }.build()
+
+    private val apiRetrofit =
+        Retrofit.Builder().baseUrl("/*BaseURL*/" /*TODO*/).addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient).build()
+
+    val sample = apiRetrofit.create(SampleAPI::class.java)
+}
