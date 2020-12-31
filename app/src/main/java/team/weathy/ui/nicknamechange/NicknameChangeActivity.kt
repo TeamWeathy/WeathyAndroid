a32f9264860f03bb3cd9eeefecc8b530345017ce
dingding-21 <phj8221@naver.com>
2020-12-31 20:18:59 +0900

    글자 수 count 활성화 구현

--- app/src/main/java/team/weathy/ui/nicknamechange/NicknameChangeActivity.kt
+++ app/src/main/java/team/weathy/ui/nicknamechange/NicknameChangeActivity.kt
@@ -1,30 +1,51 @@
 package team.weathy.ui.nicknamechange
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import team.weathy.R
 import team.weathy.databinding.ActivityNicknameChangeBinding
+import team.weathy.util.setOnDebounceClickListener
 
 
 class NicknameChangeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNicknameChangeBinding
-    private val textWatcher = object : TextWatcher{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNicknameChangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nicknameEdit.addTextChangedListener((textWatcher))
+    }
+
+    private val textWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
             val length = s.toString().length
-            binding.numOfCharacters.text = "$length/6"
+            val numOfCharactersColorMint = resources.getColor(R.color.main_mint, theme)
+            val nicknameChangeBtnActive = resources.getColor(R.color.main_mint, theme)
+            val numOfCharactersColorGrey = resources.getColor(R.color.sub_grey_6, theme)
+            val nicknameChangeBtnInactive = resources.getColor(R.color.sub_grey_3, theme)
 
-            if(length > 0){
-                val nickname_change_btn_active  = resources.getColor(R.color.main_mint)
-                binding.changeNicknameBtn.setBackgroundColor(nickname_change_btn_active)
+            binding.numOfCharacters1.text = "$length"
+            binding.numOfCharacters2.text = "/6"
+
+            if (length > 0) {
+                binding.changeNicknameBtn.isEnabled = true
+                binding.numOfCharacters1.setTextColor(numOfCharactersColorMint)
+                binding.changeNicknameBtn.setBackgroundColor(nicknameChangeBtnActive)
                 binding.nicknameEdit.setBackgroundResource(R.drawable.nickname_edit_border_active)
-
-            }
-            else{
-                val nickname_change_btn_inactive  = resources.getColor(R.color.sub_grey_3)
-                binding.changeNicknameBtn.setBackgroundColor(nickname_change_btn_inactive)
+                changeNicknameBtnClick()
+            } else {
+                binding.changeNicknameBtn.isEnabled = false
+                binding.numOfCharacters1.setTextColor(numOfCharactersColorGrey)
+                binding.changeNicknameBtn.setBackgroundColor(nicknameChangeBtnInactive)
                 binding.nicknameEdit.setBackgroundResource(R.drawable.nickname_edit_border)
             }
         }
@@ -38,15 +59,10 @@ class NicknameChangeActivity : AppCompatActivity() {
         }
 
     }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityNicknameChangeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.nicknameEdit.addTextChangedListener((textWatcher))
+    
+    private fun changeNicknameBtnClick() {
+        binding.changeNicknameBtn.setOnDebounceClickListener {
+            Toast.makeText(this, "변경되었습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
-
-
 }
