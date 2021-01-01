677f9ab8f66fb83efdca49e6b7b48ec18968f9bc
kmebin <blowhuiee@gmail.com>
2021-01-01 15:07:57 +0900

    화면 선택시 키보드 내리기

--- app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
+++ app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
@@ -1,15 +1,18 @@
 package team.weathy.ui.record.detail
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import team.weathy.R
 import team.weathy.databinding.FragmentRecordDetailBinding
 import team.weathy.util.AutoClearedValue
+import team.weathy.util.setOnDebounceClickListener
 
 class RecordDetailFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentRecordDetailBinding>()
@@ -19,6 +22,10 @@ class RecordDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.etDetail.addTextChangedListener((textWatcher))
+
+        binding.layoutDetail setOnDebounceClickListener {
+            hideKeyboard()
+        }
     }
 
     private val textWatcher = object : TextWatcher {
@@ -38,5 +45,10 @@ class RecordDetailFragment : Fragment() {
 
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         }
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.etDetail.windowToken, 0)
     }
 }
