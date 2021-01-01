371041fd21efbd7f9c87e3ee72f4583db1246ff3
mym0404 <mym0404@gmail.com>
2021-01-01 16:03:51 +0900

    CalendarView UI fix

--- app/src/main/java/team/weathy/util/BindingAdapter.kt
+++ app/src/main/java/team/weathy/util/BindingAdapter.kt
@@ -1,21 +1,41 @@
 package team.weathy.util
 
-import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
+import team.weathy.R
 
-@BindingAdapter("srcUrl", "placeholder", requireAll = false)
-fun ImageView.loadUrlAsync(url: String?, placeholder: Drawable? = null) {
-    if (url == null) {
-        Glide.with(this).load(placeholder).into(this)
-    } else {
-        Glide.with(this).load(url).apply {
-            if (placeholder != null) placeholder(placeholder)
-        }.into(this)
+@BindingAdapter("app:url", requireAll = false)
+fun ImageView.loadUrlAsync(url: String?) {
+    val anim = CircularProgressDrawable(context).apply {
+        strokeWidth = 4f
+        setColorSchemeColors(
+            *listOf(
+                R.color.main_mint, R.color.main_mint_shadow, R.color.blue_temp
+            ).map { context.getColor(it) }.toIntArray()
+        )
+        start()
     }
+
+    if (url == null) {
+        Glide.with(this).load(anim).into(this)
+    } else {
+        Glide.with(this).load(url)
+            .transition(withCrossFade(DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()))
+            .placeholder(anim).into(this)
+    }
+}
+
+@BindingAdapter("app:useCircleOutlineWithRadius")
+fun ShapeableImageView.useCircleOutlineWithRadius(radius: Float) {
+    shapeAppearanceModel = ShapeAppearanceModel().withCornerSize(radius)
 }
 
 @BindingAdapter("android:visibility")
