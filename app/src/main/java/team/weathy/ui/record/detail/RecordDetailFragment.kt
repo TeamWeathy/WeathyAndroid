f6fe2d557f144a6e921c43638b1facde7138b278
kmebin <blowhuiee@gmail.com>
2021-01-01 15:05:44 +0900

    상세 내용 텍스트 카운트 및 활성화

--- app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
+++ app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
@@ -1,10 +1,13 @@
 package team.weathy.ui.record.detail
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import team.weathy.R
 import team.weathy.databinding.FragmentRecordDetailBinding
 import team.weathy.util.AutoClearedValue
 
@@ -15,6 +18,25 @@ class RecordDetailFragment : Fragment() {
         FragmentRecordDetailBinding.inflate(layoutInflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.etDetail.addTextChangedListener((textWatcher))
+    }
 
+    private val textWatcher = object : TextWatcher {
+        override fun afterTextChanged(string: Editable?) {
+            val length = string.toString().length
+            binding.tvTextLength2.text = "$length"
+
+            if (length > 0) {
+                binding.tvTextLength2.setTextColor(resources.getColor(R.color.main_mint))
+            } else {
+                binding.tvTextLength2.setTextColor(resources.getColor(R.color.sub_grey_6))
+            }
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
     }
 }
