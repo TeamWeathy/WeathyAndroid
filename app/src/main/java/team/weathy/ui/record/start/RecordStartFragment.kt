2a8bcfeb6fc963df6cb3a2fde89b259813a32f94
kmebin <blowhuiee@gmail.com>
2021-01-01 14:38:53 +0900

    RecordLocationChangeFragment 로 이동

--- app/src/main/java/team/weathy/ui/record/start/RecordStartFragment.kt
+++ app/src/main/java/team/weathy/ui/record/start/RecordStartFragment.kt
@@ -6,13 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import team.weathy.databinding.FragmentRecordStartBinding
+import team.weathy.ui.record.RecordActivity
+import team.weathy.ui.record.locationchange.RecordLocationChangeFragment
 import team.weathy.util.AutoClearedValue
+import team.weathy.util.setOnDebounceClickListener
 
 class RecordStartFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentRecordStartBinding>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        FragmentRecordStartBinding.inflate(layoutInflater, container, false).also { binding = it }.root
+        FragmentRecordStartBinding.inflate(layoutInflater, container, false).also {
+            binding = it
+            it.btnChange setOnDebounceClickListener {
+                (activity as RecordActivity).replaceFragment(RecordLocationChangeFragment())
+            }
+        }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
