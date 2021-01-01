44ae1158975916b13ef033ccf81b78bdede61aad
mym0404 <mym0404@gmail.com>
2021-01-01 18:27:52 +0900

    GoSearch with Anim

--- app/src/main/java/team/weathy/util/AnimUtil.kt
+++ app/src/main/java/team/weathy/util/AnimUtil.kt
@@ -5,7 +5,7 @@ import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
 
 object AnimUtil {
-    fun runSpringAnimation(startValue: Float, finalValue: Float, multiplier: Float, callback: (value: Float) -> Unit) {
+    fun runSpringAnimation(startValue: Float, finalValue: Float, multiplier: Float, callback: (value: Float) -> Unit) =
         SpringAnimation(FloatValueHolder()).apply {
             setStartValue(startValue * multiplier)
             spring = SpringForce(finalValue * multiplier).apply {
@@ -14,6 +14,6 @@ object AnimUtil {
             addUpdateListener { _, value, _ ->
                 callback(value / multiplier)
             }
-        }.start()
-    }
+            start()
+        }
 }
