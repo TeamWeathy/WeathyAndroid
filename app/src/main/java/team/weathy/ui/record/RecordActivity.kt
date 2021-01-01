576125c2a2de2c528ad8d4b8ffb61231d53c9999
kmebin <blowhuiee@gmail.com>
2021-01-01 14:37:06 +0900

    RecordActivity 에 Fragment Transaction 설정

--- app/src/main/java/team/weathy/ui/record/RecordActivity.kt
+++ app/src/main/java/team/weathy/ui/record/RecordActivity.kt
@@ -2,7 +2,10 @@ package team.weathy.ui.record
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import team.weathy.databinding.ActivityRecordBinding
+import team.weathy.ui.record.start.RecordStartFragment
 
 class RecordActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecordBinding
@@ -12,5 +15,20 @@ class RecordActivity : AppCompatActivity() {
 
         binding = ActivityRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(binding.root.id, RecordStartFragment(), "record").commit()
+    }
+
+    fun replaceFragment(fragment : Fragment){
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.addToBackStack(null)
+        transaction.replace(binding.root.id, fragment, "record").commit()
+    }
+
+    fun finishFragment(fragment : Fragment){
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.remove(fragment).commit()
+        supportFragmentManager.popBackStack()
     }
 }
