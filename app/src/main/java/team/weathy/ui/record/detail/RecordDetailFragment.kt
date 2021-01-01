3a79305020e6f4e64b5f71aba40b52a8909d12e0
kmebin <blowhuiee@gmail.com>
2021-01-02 00:39:57 +0900

    기록 완료 Dialog 구현

--- app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
+++ app/src/main/java/team/weathy/ui/record/detail/RecordDetailFragment.kt
@@ -1,6 +1,11 @@
 package team.weathy.ui.record.detail
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.InsetDrawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,9 +13,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import team.weathy.R
 import team.weathy.databinding.FragmentRecordDetailBinding
+import team.weathy.ui.main.MainActivity
 import team.weathy.util.AutoClearedValue
 import team.weathy.util.setOnDebounceClickListener
 
@@ -27,6 +34,8 @@ class RecordDetailFragment : Fragment() {
             hideKeyboard()
             binding.etDetail.clearFocus()
         }
+
+        setDialogClickListener()
     }
 
     private val textWatcher = object : TextWatcher {
@@ -51,5 +60,33 @@ class RecordDetailFragment : Fragment() {
     private fun hideKeyboard() {
         val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(binding.etDetail.windowToken, 0)
+    }
+
+    private fun setDialogClickListener() {
+        binding.btnConfirm setOnDebounceClickListener  {
+            val dialog = AlertDialog.Builder(context).create()
+            val edialog: LayoutInflater = LayoutInflater.from(context)
+            val mView: View = edialog.inflate(R.layout.dialog_record_complete, null)
+
+            val confirm: Button = mView.findViewById(R.id.btn_confirm)
+
+            confirm setOnDebounceClickListener {
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+            // Dialog Background 설정
+            val color = ColorDrawable(Color.TRANSPARENT)
+            // Dialog 크기 설정
+            val inset = InsetDrawable(color, 66)
+            dialog.window?.setBackgroundDrawable(inset)
+
+            dialog.setView(mView)
+            dialog.create()
+            dialog.show()
+        }
     }
 }
