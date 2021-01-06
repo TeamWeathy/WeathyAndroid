d3c869048262a75c1a30f12d9e4977e988052edb
mym0404 <mym0404@gmail.com>
2021-01-06 21:54:08 +0900

    RecordClothesSelectFragment.kt 탭 선택 로직

--- app/src/main/java/team/weathy/ui/record/RecordViewModel.kt
+++ app/src/main/java/team/weathy/ui/record/RecordViewModel.kt
@@ -1,7 +1,28 @@
 package team.weathy.ui.record
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RecordViewModel : ViewModel() {
+    private val _selectedClothesTabIndex = MutableLiveData(0)
+    val selectedClothesTabIndex: LiveData<Int> = _selectedClothesTabIndex
 
+    val topClothes = MutableLiveData(listOf("상의1", "상의2"))
+    val selectedTopClothes = MutableLiveData(listOf<String>())
+
+    val bottomClothes = MutableLiveData(listOf("하의1", "하의2"))
+    val selectedBottomClothes = MutableLiveData(listOf<String>())
+
+    val outerClothes = MutableLiveData(listOf("아우터1", "아우터2"))
+    val selectedOuterClothes = MutableLiveData(listOf<String>())
+
+    val etcClothes = MutableLiveData(listOf("기타1", "기타2"))
+    val selectedEtcClothes = MutableLiveData(listOf<String>())
+
+    fun changeSelectedClothesTabIndex(tab: Int) {
+        if (selectedClothesTabIndex.value != tab) {
+            _selectedClothesTabIndex.value = tab
+        }
+    }
 }
--- app/src/main/java/team/weathy/ui/record/clothesselect/RecordClothesSelectFragment.kt
+++ app/src/main/java/team/weathy/ui/record/clothesselect/RecordClothesSelectFragment.kt
@@ -4,21 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import team.weathy.databinding.FragmentRecordClothesSelectBinding
 import team.weathy.ui.record.RecordActivity
+import team.weathy.ui.record.RecordViewModel
 import team.weathy.util.AutoClearedValue
 import team.weathy.util.setOnDebounceClickListener
 
 
 class RecordClothesSelectFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentRecordClothesSelectBinding>()
+    private val viewModel by activityViewModels<RecordViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         FragmentRecordClothesSelectBinding.inflate(layoutInflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         configureClothesSelectNavigation()
+
+        configureTabs()
     }
 
     private fun configureClothesSelectNavigation() {
@@ -31,6 +38,40 @@ class RecordClothesSelectFragment : Fragment() {
         binding.add.setOnLongClickListener {
             (activity as? RecordActivity)?.navigateClothesSelectToClothesDelete()
             true
+        }
+    }
+
+    private fun configureTabs() {
+        setOnTabClickListeners()
+        viewModel.selectedClothesTabIndex.observe(viewLifecycleOwner) { tab ->
+            selectTab(tab)
+        }
+    }
+
+    private fun setOnTabClickListeners() {
+        binding.layoutTop setOnDebounceClickListener {
+            viewModel.changeSelectedClothesTabIndex(0)
+        }
+        binding.layoutBottom setOnDebounceClickListener {
+            viewModel.changeSelectedClothesTabIndex(1)
+        }
+        binding.layoutOuter setOnDebounceClickListener {
+            viewModel.changeSelectedClothesTabIndex(2)
+        }
+        binding.layoutEtc setOnDebounceClickListener {
+            viewModel.changeSelectedClothesTabIndex(3)
+        }
+    }
+
+    private fun selectTab(tab: Int) {
+        val dividers = listOf(
+            binding.divider,
+            binding.divider2,
+            binding.divider3,
+            binding.divider4,
+        )
+        dividers.forEachIndexed { index, view ->
+            view.isInvisible = index != tab
         }
     }
 }
