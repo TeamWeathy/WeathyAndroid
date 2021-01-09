d5522629dfbcdd1913e5c3210e3decb5e0841a29
mym0404 <mym0404@gmail.com>
2021-01-09 15:57:24 +0900

    SearchViewModel

--- app/src/main/java/team/weathy/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/team/weathy/ui/main/search/SearchViewModel.kt
@@ -7,28 +7,46 @@ import team.weathy.api.WeatherAPI
 import team.weathy.di.ApiMock
 import team.weathy.model.entity.OverviewWeather
 import team.weathy.util.dateHourString
+import team.weathy.util.extensions.MediatorLiveData
+import team.weathy.util.extensions.addSources
 import team.weathy.util.extensions.launchCatch
 import java.time.LocalDateTime
 
 class SearchViewModel @ViewModelInject constructor(@ApiMock private val weatherAPI: WeatherAPI) : ViewModel() {
     val focused = MutableLiveData(false)
     val query = MutableLiveData("")
-    val isRecentItemShown = MutableLiveData(true)
+    val loading = MutableLiveData(false)
+
+    val recentlySearchResult = MutableLiveData<List<OverviewWeather>>(listOf())
     val searchResult = MutableLiveData<List<OverviewWeather>>(listOf())
-    val showEmpty = MutableLiveData(true)
 
-    fun search() = launchCatch({
-        weatherAPI.searchWeather(keyword = query.value!!, dateOrHourStr = LocalDateTime.now().dateHourString)
-    }, onSuccess = {
-        if (it.list == null) {
-            searchResult.value = listOf()
-            showEmpty.value = true
-        } else {
-            searchResult.value = it.list
-            showEmpty.value = false
+    val showRecently = MediatorLiveData(true) {
+        addSources(focused, query) { focused, query ->
+            value = !focused && query.isEmpty()
         }
+    }
+    val showEmpty = MediatorLiveData(true) {
+        addSources(
+            showRecently, recentlySearchResult, searchResult
+        ) { showRecently, recentlySearchResult, searchResult ->
+            value = if (showRecently) recentlySearchResult.isEmpty() else searchResult.isEmpty()
+        }
+    }
 
-    })
+    fun search() {
+        launchCatch({
+            weatherAPI.searchWeather(keyword = query.value!!, dateOrHourStr = LocalDateTime.now().dateHourString)
+        }, loading, onSuccess = {
+            if (it.list == null) {
+                searchResult.value = listOf()
+                showEmpty.value = true
+            } else {
+                searchResult.value = it.list
+                showEmpty.value = false
+            }
+
+        })
+    }
 
     fun onItemRemoved(position: Int) {
         searchResult.value = searchResult.value!!.toMutableList().apply {
@@ -36,3 +54,4 @@ class SearchViewModel @ViewModelInject constructor(@ApiMock private val weatherA
         }
     }
 }
+
