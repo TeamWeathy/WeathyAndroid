cd4f94d62b1347cfc2612016887c6965dd296cae
mym0404 <mym0404@gmail.com>
2021-01-09 23:09:10 +0900

    HomeFragment 중복 제거

--- app/src/main/java/team/weathy/ui/main/home/HomeFragment.kt
+++ app/src/main/java/team/weathy/ui/main/home/HomeFragment.kt
@@ -5,16 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import team.weathy.R
 import team.weathy.databinding.FragmentHomeBinding
 import team.weathy.util.AutoClearedValue
+import team.weathy.util.extensions.getColor
+import team.weathy.util.setOnDebounceClickListener
 
 class HomeFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentHomeBinding>()
+    var cnt = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         FragmentHomeBinding.inflate(layoutInflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding.weathyQuestion.setOnDebounceClickListener {
+            if (cnt == 0) {
+                binding.homeScroll.setBackgroundColor(getColor(R.color.transparent_black))
+                cnt++
+            } else {
+                binding.homeScroll.setBackgroundColor(getColor(R.color.transparent))
+                cnt--
+            }
+        }
     }
 }
