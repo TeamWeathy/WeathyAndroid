74970acb8e71fef361046d1a9f9c857c72edb2c0
mym0404 <mym0404@gmail.com>
2021-01-10 01:41:21 +0900

    HomeFragment Animation 구현

--- app/src/main/java/team/weathy/ui/main/home/HomeFragment.kt
+++ app/src/main/java/team/weathy/ui/main/home/HomeFragment.kt
@@ -4,25 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
+import team.weathy.R
 import team.weathy.databinding.FragmentHomeBinding
 import team.weathy.util.AutoClearedValue
+import team.weathy.util.debugE
 import team.weathy.util.setOnDebounceClickListener
 
 class HomeFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentHomeBinding>()
-    var cnt = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
         FragmentHomeBinding.inflate(layoutInflater, container, false).also { binding = it }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        binding.weathyQuestion.setOnDebounceClickListener {
-//            if (cnt == 0) {
-//                cnt++
-//            } else {
-//                cnt--
-//            }
-//        }
+
+        binding.container.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                if (p2 != R.layout.scene_home_third) binding.bg.crossfade = p3
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                debugE(p1)
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+        })
+
+        binding.downArrow setOnDebounceClickListener {
+            binding.container.transitionToState(R.layout.scene_home_second)
+        }
     }
 }
