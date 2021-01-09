f337c91b9abe4878ec62af65b5bef9bb747efaa5
mym0404 <mym0404@gmail.com>
2021-01-10 02:09:59 +0900

    데이터베이스 exportSchemes = false

--- app/src/main/java/team/weathy/database/AppDatabase.kt
+++ app/src/main/java/team/weathy/database/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import team.weathy.model.entity.RecentSearchCode
 
-@Database(entities = [RecentSearchCode::class], version = 1)
+@Database(entities = [RecentSearchCode::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun recentSearchCodeDao(): RecentSearchCodeDao
 }
--- app/src/main/java/team/weathy/view/WeeklyWeatherView.kt
+++ app/src/main/java/team/weathy/view/WeeklyWeatherView.kt
@@ -2,19 +2,35 @@ package team.weathy.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.LinearLayout
+import team.weathy.R
+import team.weathy.databinding.ItemHourlyWeatherBinding
 
 class WeeklyWeatherView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
     LinearLayout(context, attrs) {
 
-
+    private val weatherItems = (0..6).map {
+        ItemHourlyWeatherBinding.bind(LayoutInflater.from(context).inflate(R.layout.item_hourly_weather, this, false))
+            .also {
+                it.root.layoutParams = LayoutParams(0, WRAP_CONTENT, 1f)
+            }
+    }
 
     init {
-
+        initContainer()
+        addViews()
     }
 
     private fun initContainer() {
-        orientation = LinearLayout.HORIZONTAL
+        orientation = HORIZONTAL
         weightSum = 7f
+    }
+
+    private fun addViews() {
+        weatherItems.forEach {
+            addView(it.root)
+        }
     }
 }
