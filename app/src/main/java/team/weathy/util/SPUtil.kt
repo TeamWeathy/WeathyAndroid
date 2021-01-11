ed38d48334be9bb57b5aeba2f1c142b2d1f80f77
mym0404 <mym0404@gmail.com>
2021-01-11 18:55:21 +0900

    Fix SPUtil with default SharedPreferences callback

--- app/src/main/java/team/weathy/util/SPUtil.kt
+++ app/src/main/java/team/weathy/util/SPUtil.kt
@@ -1,16 +1,24 @@
 package team.weathy.util
 
 import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV
 import androidx.security.crypto.EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
 import androidx.security.crypto.MasterKey
 
 class SPUtil(context: Application) {
-    private val masterKey = MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
-    val sharedPreferences = EncryptedSharedPreferences.create(
-        context, SP_NAME, masterKey, AES256_SIV, AES256_GCM
-    )
+    private val masterKey: MasterKey? = try {
+        MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
+    } catch (e: Throwable) {
+        null
+    }
+    val sharedPreferences: SharedPreferences = masterKey?.let {
+        EncryptedSharedPreferences.create(
+            context, SP_NAME, masterKey, AES256_SIV, AES256_GCM
+        )
+    } ?: context.getSharedPreferences(SP_NAME, MODE_PRIVATE)
 
     var isFirstLaunch: Boolean
         get() {
