bcf4420dff17989ff192199e39541b7eaeaf58c1
dingding-21 <phj8221@naver.com>
2021-01-14 08:56:02 +0900

    viewModel 써서 변경

--- app/src/main/java/team/weathy/ui/nicknamechange/NicknameChangeActivity.kt
+++ app/src/main/java/team/weathy/ui/nicknamechange/NicknameChangeActivity.kt
@@ -1,140 +1,53 @@
 package team.weathy.ui.nicknamechange
 
-import android.animation.AnimatorInflater
-import android.animation.AnimatorSet
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.MotionEvent
-import android.view.View
-import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import team.weathy.R
+import androidx.lifecycle.observe
+import dagger.hilt.android.AndroidEntryPoint
 import team.weathy.databinding.ActivityNicknameChangeBinding
-import team.weathy.util.extensions.showToast
+import team.weathy.ui.main.MainActivity
+import team.weathy.util.extensions.hideKeyboard
 import team.weathy.util.setOnDebounceClickListener
 
-
+@AndroidEntryPoint
 class NicknameChangeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNicknameChangeBinding
+    private val viewModel by viewModels<NicknameChangeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityNicknameChangeBinding.inflate(layoutInflater)
+        binding = ActivityNicknameChangeBinding.inflate(layoutInflater).also {
+            it.vm = viewModel
+            it.lifecycleOwner = this
+        }
         setContentView(binding.root)
 
-        binding.nicknameEdit.addTextChangedListener(textWatcher)
-        exitNicknameChange()
-        checkBlank()
-        checkBackground()
+        observeViewModel()
+        deleteNickname()
     }
 
-    private val textWatcher = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-            val length = s.toString().length
-            binding.numOfCharacters1.text = "$length"
-            binding.numOfCharacters2.text = "/6"
 
-            if (length > 0) {
-                binding.deleteNicknameBtn.isEnabled = true
-                binding.numOfCharacters1.setTextColor(getColor(R.color.main_mint))
-                binding.nicknameEdit.setBackgroundResource(R.drawable.edit_border_active)
-                binding.deleteNicknameBtn.visibility = View.VISIBLE
-                deleteNickname()
-                changeNicknameBtnClick()
-                if (!binding.changeNicknameBtn.isEnabled)
-                    setButtonEnabled(true)
-            } else {
-                binding.deleteNicknameBtn.isEnabled = false
-                binding.numOfCharacters1.setTextColor(getColor(R.color.sub_grey_6))
-                binding.nicknameEdit.setBackgroundResource(R.drawable.edit_border)
-                binding.deleteNicknameBtn.visibility = View.INVISIBLE
-                if (binding.changeNicknameBtn.isEnabled)
-                    setButtonDisabled(false)
-            }
-
+    private fun observeViewModel() {
+        viewModel.onHideKeyboard.observe(this) {
+            hideKeyboard()
         }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
+        viewModel.onSuccess.observe(this) {
+            navigateMain()
         }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-        }
-
     }
 
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-        if (currentFocus != null) {
-            inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-        }
-        return super.dispatchTouchEvent(ev)
-    }
-
-    private fun changeNicknameBtnClick() {
-        binding.changeNicknameBtn.setOnDebounceClickListener {
-            showToast("닉네임이 변경되었어요!")
-            finish()
-        }
+    private fun navigateMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     private fun deleteNickname() {
         binding.deleteNicknameBtn.setOnDebounceClickListener {
-            binding.nicknameEdit.setText("")
+            binding.nicnknameEdit.setText("")
         }
-    }
-
-    private fun exitNicknameChange() {
-        binding.exitNicknameChangeBtn.setOnDebounceClickListener {
-            finish()
-        }
-    }
-
-    private fun setCountVisibility(hasFocus: Boolean) {
-        binding.numOfCharacters1.isVisible = hasFocus
-        binding.numOfCharacters2.isVisible = hasFocus
-    }
-
-    private fun checkBlank() {
-        binding.nicknameEdit.setOnFocusChangeListener { _, hasFocus ->
-            setCountVisibility(hasFocus)
-
-            var getEdit = binding.nicknameEdit.text.toString()
-            if (getEdit.equals("")) {
-                binding.nicknameEdit.setBackgroundResource(R.drawable.edit_border)
-                binding.deleteNicknameBtn.visibility = View.INVISIBLE
-            } else {
-                binding.nicknameEdit.setBackgroundResource(R.drawable.edit_border_active)
-                binding.deleteNicknameBtn.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun checkBackground() {
-        binding.layoutNickname.setOnDebounceClickListener {
-            binding.nicknameEdit.clearFocus()
-            binding.nicknameEdit.setBackgroundResource(R.drawable.edit_border)
-            binding.deleteNicknameBtn.visibility = View.INVISIBLE
-        }
-    }
-
-    private fun setButtonEnabled(isEnable: Boolean) {
-        val colorChangeActive = AnimatorInflater.loadAnimator(this, R.animator.color_change_active_anim) as AnimatorSet
-        colorChangeActive.setTarget(binding.changeNicknameBtn)
-        colorChangeActive.start()
-        binding.changeNicknameBtn.isEnabled = isEnable
-    }
-
-    private fun setButtonDisabled(isEnable: Boolean) {
-        val colorChange = AnimatorInflater.loadAnimator(this, R.animator.color_change_anim) as AnimatorSet
-        colorChange.setTarget(binding.changeNicknameBtn)
-        colorChange.start()
-        binding.changeNicknameBtn.isEnabled = isEnable
     }
 }
+
