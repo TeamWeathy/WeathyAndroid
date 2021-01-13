145052a1488b61327b70ecf8426a34174bd0c830
kmebin <blowhuiee@gmail.com>
2021-01-13 23:02:42 +0900

    RecordViewModel 수정

--- app/src/main/java/team/weathy/ui/record/RecordViewModel.kt
+++ app/src/main/java/team/weathy/ui/record/RecordViewModel.kt
@@ -1,64 +1,24 @@
 package team.weathy.ui.record
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import team.weathy.model.entity.OverviewWeather
+import team.weathy.api.ClothesAPI
+import team.weathy.di.Api
 import team.weathy.util.EventLiveData
-import team.weathy.util.debugE
 import team.weathy.util.extensions.updateList
 import team.weathy.util.extensions.updateSet
 
-class RecordViewModel : ViewModel() {
+class RecordViewModel @ViewModelInject constructor(
+    @Api private val clothesAPI: ClothesAPI,
+) : ViewModel() {
     private val _choicedClothesTabIndex = MutableLiveData(0)
     val choicedClothesTabIndex: LiveData<Int> = _choicedClothesTabIndex
 
     val clothesPairs = listOf<Pair<MutableLiveData<List<String>>, MutableLiveData<Set<Int>>>>(
-        MutableLiveData(
-            listOf(
-                "니트", "후드티", "티셔츠", "하이",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-                "더미asdasdasdasdasdasdasd",
-
-
-                )
-        ) to MutableLiveData(setOf()),
+        MutableLiveData(listOf("니트", "후드티", "티셔츠")) to MutableLiveData(setOf()),
         MutableLiveData(listOf("데님팬츠", "스커트", "슬랙스")) to MutableLiveData(setOf()),
         MutableLiveData(listOf("패딩", "코트", "점퍼")) to MutableLiveData(setOf()),
         MutableLiveData(listOf("목도리", "장갑", "모자")) to MutableLiveData(setOf()),
@@ -75,6 +35,17 @@ class RecordViewModel : ViewModel() {
         }
     }
     val selectedClothes = MediatorLiveData<Set<Int>>().apply {
+        value = clothesPairs[0].second.value!!
+        clothesPairs.forEach {
+            addSource(it.second) { set ->
+                value = set
+            }
+        }
+        addSource(choicedClothesTabIndex) {
+            value = clothesPairs[it].second.value!!
+        }
+    }
+    val selectedClothesForDelete = MediatorLiveData<Set<Int>>().apply {
         value = clothesPairs[0].second.value!!
         clothesPairs.forEach {
             addSource(it.second) { set ->
@@ -138,9 +109,5 @@ class RecordViewModel : ViewModel() {
             selectedClothesCount += clothesPairs[i].second.value!!.size
         }
         return selectedClothesCount
-    }
-
-    fun onLocationChanged(weather: OverviewWeather) {
-        debugE("hi")
     }
 }
