8463cc4837ab35cf8950763aa5e416ac1ba51cf5
mym0404 <mym0404@gmail.com>
2021-01-13 19:39:23 +0900

    Modify Extra enum

--- app/src/main/java/team/weathy/model/entity/WeatherRain.kt
+++ app/src/main/java/team/weathy/model/entity/WeatherRain.kt
@@ -10,7 +10,8 @@ import java.lang.reflect.Type
 
 @JsonAdapter(WeatherRainSerializer::class)
 enum class WeatherRain(val id: Int, val representation: String) {
-    LEVEL1(1, "적게옴"), LEVEL2(2, "조금적게옴"), LEVEL3(3, "보통"), LEVEL4(4, "조금많이옴"), LEVEL5(5, "많이옴"),
+    LEVEL1(1, "매우 조금"), LEVEL2(2, "조금"), LEVEL3(3, "보통"), LEVEL4(4, "다소 많음"), LEVEL5(5, "많음"), LEVEL6(6, "매우 많음")
+
 
     ;
 
