ae111431ac2c07af3c2518af22576d474d2b2d91
dingding-21 <phj8221@naver.com>
2021-03-01 16:15:18 +0900

    설정뷰 layout 수정

--- app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
+++ app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import team.weathy.databinding.ActivitySettingBinding
 import team.weathy.ui.developerinfo.DeveloperInfoActivity
-import team.weathy.ui.inquire.InquireActivity
 import team.weathy.ui.nicknamechange.NicknameChangeActivity
-import team.weathy.util.extensions.showToast
 import team.weathy.util.setOnDebounceClickListener
 
 
@@ -21,38 +19,21 @@ class SettingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         goNicknameChange()
-        goInquire()
         goDeveloperInfo()
-        pushNotifSwitchAct()
         exitSetting()
     }
 
     private fun goNicknameChange() {
-        binding.changeNicknameClick.setOnDebounceClickListener {
+        binding.nicknameChangeClick.setOnDebounceClickListener {
             val intent = Intent(this, NicknameChangeActivity::class.java)
             startActivity(intent)
         }
     }
 
-    private fun goInquire() {
-        binding.inquireClick.setOnDebounceClickListener {
-            val intent = Intent(this, InquireActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
     private fun goDeveloperInfo() {
-        binding.developerInfoClick.setOnDebounceClickListener {
+        binding.developerClick.setOnDebounceClickListener {
             val intent = Intent(this, DeveloperInfoActivity::class.java)
             startActivity(intent)
-        }
-    }
-
-    private fun pushNotifSwitchAct() {
-        binding.pushNotifSwitch.setOnCheckedChangeListener { _, onSwitch ->
-            if (onSwitch) {
-                showToast(" 현재 위치에 따라 날씨를 알려드릴게요!")
-            }
         }
     }
 
