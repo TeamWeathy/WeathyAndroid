debe67cce0090f7a6e5d4630ec06902478df73b9
dingding-21 <phj8221@naver.com>
2021-03-01 17:01:50 +0900

    개인정보, 위치기반 notion으로 연결

--- app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
+++ app/src/main/java/team/weathy/ui/setting/SettingActivity.kt
@@ -1,6 +1,7 @@
 package team.weathy.ui.setting
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import team.weathy.R
@@ -19,38 +20,46 @@ class SettingActivity : AppCompatActivity() {
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        goNicknameChange()
-        goDeveloperInfo()
-        goInquire()
-        exitSetting()
+        configureNavigation()
     }
 
-    private fun goNicknameChange() {
+    private fun configureNavigation() {
+
         binding.nicknameChangeClick.setOnDebounceClickListener {
             val intent = Intent(this, NicknameChangeActivity::class.java)
             startActivity(intent)
-        }
-    }
 
-    private fun goDeveloperInfo() {
+        }
         binding.developerClick.setOnDebounceClickListener {
             val intent = Intent(this, DeveloperInfoActivity::class.java)
             startActivity(intent)
         }
-    }
 
-    private fun exitSetting() {
         binding.exitSettingBtn.setOnDebounceClickListener {
             finish()
         }
-    }
 
-    private fun goInquire() {
         binding.inquireClick.setOnDebounceClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_receiver)))
             intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_title))
             intent.type = "message/rfc822"
+            startActivity(intent)
+        }
+
+        binding.infoClick.setOnDebounceClickListener {
+            var intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.notion.so/5a2f164b5afe46559c78d7efed6d3e8a")
+            )
+            startActivity(intent)
+        }
+
+        binding.locationClick.setOnDebounceClickListener {
+            var intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.notion.so/69010f5bca1e443ca3b47cfd74bdb41f")
+            )
             startActivity(intent)
         }
     }
