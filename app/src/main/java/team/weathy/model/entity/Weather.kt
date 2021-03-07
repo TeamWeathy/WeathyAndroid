3dc16da6792024e8fa3639f408cc2cd8ef42c74f
dingding-21 <phj8221@naver.com>
2021-03-07 15:33:11 +0900

    Fix GetWeathy IconId

--- app/src/main/java/team/weathy/model/entity/Weather.kt
+++ app/src/main/java/team/weathy/model/entity/Weather.kt
@@ -54,7 +54,7 @@ enum class Weather(
     ),
     SHOWER_RAIN(
             9,
-            R.drawable.ic_showerrain,
+            R.drawable.ic_showerrain_day,
             R.drawable.main_img_showerrain_day,
             R.drawable.record_img_showerrain_day,
             R.drawable.main_bg_snowrain_day,
@@ -63,7 +63,7 @@ enum class Weather(
     ),
     RAIN(
             10,
-            R.drawable.ic_rain_day,
+            R.drawable.ic_rain,
             R.drawable.main_img_rain_day,
             R.drawable.record_img_rain,
             R.drawable.main_bg_snowrain_day,
@@ -130,7 +130,7 @@ enum class Weather(
     ),
     SHOWER_RAIN_NIGHT(
             109,
-            R.drawable.ic_showerrain,
+            R.drawable.ic_showerrain_night,
             R.drawable.main_img_showerrain_night,
             R.drawable.record_img_showerrain_night,
             R.drawable.main_bg_snowrain_night,
@@ -147,7 +147,7 @@ enum class Weather(
     ),
     RAIN_NIGHT(
             110,
-            R.drawable.ic_rain_night,
+            R.drawable.ic_rain,
             R.drawable.main_img_rain_night,
             R.drawable.record_img_rain,
             R.drawable.main_bg_snowrain_night,
